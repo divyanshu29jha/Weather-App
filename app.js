294723637f451e5a17da1feb5a0dfeea58ee3af6
DJ-IIITS <divyanshu2003dj@gmail.com>
2023-03-20 10:33:33 +0530

    made backend and used API

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express");		// npm i express
+const bodyParser = require("body-parser");	// npm i body-parser
+const https = require("https");
+
+const app = express();
+
+app.set("view engine", "ejs");  // npm i ejs
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let city = "-", tmp= "-", maxTmp= "-", minTmp= "-", humid= "-", precip= "-", feel= "-", wind= "-", sunR= "-", sunS= "-";
+
+app.get("/", (req, res) => {
+	res.render("index", {
+		yourCity: city,
+		temp: tmp,
+		maxTemp: maxTmp,
+		minTemp: minTmp,
+		humidity: humid,
+		pct: precip,
+		feelsLike: feel,
+		windSpeed: wind,
+		sunrise: sunR,
+		sunset: sunS
+	});
+})
+
+app.post("/", (req, res) => {
+
+	const myCity = req.body.cityName;	// using body-parser, attribute name="cityName" in the form.
+	const options = {
+		"method": "GET",
+		"hostname": "weather-by-api-ninjas.p.rapidapi.com",
+		"port": null,
+		"path": "/v1/weather?city=" + myCity,
+		"headers": {
+			"X-RapidAPI-Key": "913a2f7725msh624c7d1603a1295p1be896jsn67cfa03ef040",
+			"X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+			"useQueryString": true
+		}
+	};
+
+	let data="";
+	const request = https.request(options, function (response) {
+		const chunks = [];
+
+		response.on("data", function (chunk) {
+			chunks.push(chunk);
+		});
+
+		response.on("end", function () {
+			const body = Buffer.concat(chunks);
+			let str = body.toString();
+			data = JSON.parse(str);
+			city = myCity;
+			tmp = data.temp;
+			maxTmp = data.max_temp;
+			minTmp = data.min_temp,
+			humid = data.humidity;
+			precip = data.cloud_pct;
+			feel = data.feels_like;		
+			wind = data.wind_speed;
+			sunR = data.sunrise;
+		    sunS = data.sunset;
+			res.redirect("/");
+		});	
+		
+	});
+	
+	request.end();
+	
+})
+
+app.listen(3000, () => {
+	console.log("Server is running on port 3000...");
+})
